ac5d7d75f4cb59f89c6c0e93a9eb0f388ecd9e7a
betomelo_9 <betomelo_9@hotmail.com>
2023-09-06 19:54:41 -0300

    Aula camadas inclusao da API

---
+++ src/LOJAH1.Catalogo.API/Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using LOJAH1.Catalogo.Application.Interfaces;
+using LOJAH1.Catalogo.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOJAH1.Catalogo.API.Controllers
+{
+
+	[ApiController]
+	[Route("[controller]")]
+	public class ProdutoController : ControllerBase
+	{
+		private readonly IProdutoService _produtoService;
+
+		public ProdutoController(IProdutoService produtoService)
+		{
+			_produtoService = produtoService;
+		}
+
+		[HttpPost]
+		public IActionResult Post(NovoProdutoViewModel novoProdutoViewModel)
+		{
+			_produtoService.Adicionar(novoProdutoViewModel);
+			return Ok("Registro adicionado com sucesso!");
+		}
+	}
+}
